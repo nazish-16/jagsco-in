fa8456a78d54665bfeb3925900fb56cd88f41808
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2024-07-18 01:21:26 +0530

    major homepage changes

--- Components/Homepage.tsx
+++ Components/Homepage.tsx
@@ -1,22 +1,53 @@
-import React from 'react';
+'use client'
 
+import React from 'react';
+import { useState } from "react";
+
+const images = [
+    "https://images.pexels.com/photos/2117938/pexels-photo-2117938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/2341290/pexels-photo-2341290.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/8746906/pexels-photo-8746906.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/2117937/pexels-photo-2117937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/866351/pexels-photo-866351.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+];
 const Homepage = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const nextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    const prevSlide = () => {
+        setCurrentIndex(
+            (prevIndex) => (prevIndex - 1 + images.length) % images.length
+        );
+    };
     return (
-        <div className='flex flex-col items-center justify-center'>
-            <section className="text-white py-20 relative top-[130px] sm:top-[180px]">
-                <div className="container mx-auto px-4">
-                    <div className="flex flex-col items-center justify-center">
-                        <h1 className="text-3xl md:text-5xl font-bold text-center mb-8">Experience <span className='text-[#eee] underline underline-offset-1'>Jagsco Interiors</span></h1>
-                        <p className="text-md md:text-lg text-center mb-8 max-w-3xl xl:max-w-5xl">Jagsco Interiors, your premier destination for exquisite marble and stone solutions. At Jagsco, we specialize in sourcing, crafting, and delivering the finest quality marble and stone products to transform your spaces into timeless masterpieces.</p>
-                        <div>
-                            <a href="#context" className="bg-white text-black px-10 py-3 rounded-lg font-medium transition duration-300 ease-in-out hover:bg-opacity-70 m-4">Explore</a>
+        <div>
+            <div className='flex flex-row items-center justify-center'>
+                <section className="text-white py-20 relative top-[130px] sm:top-[80px] left-0 lg:left-[40px] flex-col flex items-center justify-center">
+                    <div className="container mx-auto px-4">
+                        <h1 className="text-3xl md:text-5xl font-bold mb-8 text-center xl:text-start"><span className='text-[#eee]'>Jagsco Interiors</span></h1>
+                        <p className='max-w-3xl text-[15px] mb-8 text-center xl:text-start'>Jagsco Marble is a premier provider of high-quality marble for residential and commercial projects. We pride ourselves on offering an extensive selection of elegant and durable marble to elevate the aesthetics of any space.</p>
+                        <p className='hidden lg:flex max-w-3xl text-[15px] mb-14 text-center xl:text-start'>Our products are sourced from the finest quarries, ensuring top-notch quality and timeless beauty. Whether you're renovating a home, designing a luxurious office, or creating a stunning commercial environment, Jagsco Marble delivers exceptional craftsmanship and unparalleled customer service.</p>
+                        <div className='flex flex-row items-center justify-center xl:items-start xl:justify-start'>
+                            <a href="#context" className="bg-white text-black px-12 py-3 tracking-wider rounded-full font-medium transition duration-300 ease-in-out hover:bg-opacity-70 mr-5">Explore</a>
+                            <a href="#context" className="bg-white text-black px-12 py-3 tracking-wider rounded-full font-medium transition duration-300 ease-in-out hover:bg-opacity-70 mr-5">Shop</a>
                         </div>
                     </div>
-                </div>
-            </section>
-            <section className='relative bottom-[-270px] sm:bottom-[-380px] items-center justify-center flex flex-col animate-pulse'>
-                <h1 className='text-white text-md xl:text-lg'>Scroll down</h1>
-                <img src="https://img.icons8.com/?size=100&id=85018&format=png&color=FFFFFF" alt="down-arrow" width={30} className='top-1 relative animate-bounce' />
+                </section>
+                <section>
+                    <div className="hidden relative w-full mx-auto xl:flex items-center justify-center top-[110px] left-[50px] mr-36">
+                        <div className="flex items-center justify-center">
+                            <img src="https://images.pexels.com/photos/2117938/pexels-photo-2117938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="homepage-bgs" width={440} className='relative left-20 bottom-20 rounded-lg hover:scale-110 transition-transform ease-in-out duration-100' />
+                            <img src="https://images.pexels.com/photos/8746906/pexels-photo-8746906.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="homepage-bgs" width={300} className='relative right-20 top-6 rounded-lg hover:scale-110 transition-transform ease-in-out duration-100' />
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <section className='flex flex-col justify-center items-center relative top-64 sm:top-36'>
+                <h1 className='text-white animate-pulse'>Scroll down</h1>
+                <img src="https://img.icons8.com/?size=100&id=85123&format=png&color=FFFFFF" alt="scroll-down" className='w-8 animate-bounce' />
             </section>
         </div>
     );
--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -13,82 +13,114 @@ const Navbar = () => {
     };
 
     return (
-        <nav className={`relative top-0 left-0 w-full z-50 opacity-100 transition-colors duration-300`} id='home'>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-16">
-                    <div className="flex items-center">
-                        <a href="#">
-                            <Image src={icon} alt="icon" width={30} height={30} />
-                        </a>
-                        <div className="ml-2 md:block">
-                            <a className="text-white text-xl font-bold" href="#">Jagsco Interiors</a>
+        <div>
+            <div className='bg-black h-10 justify-center items-center flex p-7'>
+                <h1 className='text-white tracking-widest'> <span className='font-light'>Experience 📦</span> <span className='font-semibold'>Jagsco Interiors</span></h1>
+            </div>
+            <div className='hidden lg:flex flex-row items-center justify-between p-1'>
+                <div className='flex flex-row items-center justify-center'>
+                    <Link href="#">
+                        <img src="https://img.icons8.com/?size=100&id=60440&format=png&color=FFFFFF" alt="" className='w-7 m-1 hover:translate-y-[-2px] transition ease duration-200' />
+                    </Link>
+                    <Link href="#">
+                        <img src="https://img.icons8.com/?size=100&id=32309&format=png&color=FFFFFF" alt="" className='w-7 m-1 hover:translate-y-[-2px] transition ease duration-200' />
+                    </Link>
+                    <Link href="#">
+                        <img src="https://img.icons8.com/?size=100&id=16733&format=png&color=FFFFFF" alt="" className='w-7 m-1 hover:translate-y-[-2px] transition ease duration-200' />
+                    </Link>
+                </div>
+                <div className='flex flex-row'>
+                    <ul className='flex p-3'>
+                        <li><Link href="#" className='m-5 text-white hover:text-opacity-60 transition duration-300 ease-in-out'>Home</Link></li>
+                        <li><Link href="#about" className='m-5 text-white hover:text-opacity-60 transition duration-300 ease-in-out'>About us</Link></li>
+                        <li><Link href="#context" className='m-5 text-white hover:text-opacity-60 transition duration-300 ease-in-out'>Services</Link></li>
+                        <li><Link href="#projects" className='m-5 text-white hover:text-opacity-60 transition duration-300 ease-in-out'>Projects</Link></li>
+                        <li><Link href="#brands" className='m-5 text-white hover:text-opacity-60 transition duration-300 ease-in-out'>Brands</Link></li>
+                        <li><Link href="#contact" className='m-5 text-white hover:text-opacity-60 transition duration-300 ease-in-out'>Contact us</Link></li>
+                    </ul>
+                </div>
+                <div className='hidden xl:flex'>
+                    <a href="#contact" className="bg-white text-black px-4 py-1 rounded-full font-medium transition duration-300 ease-in-out bg-opacity-70 mr-5 hover:bg-opacity-100">Contact</a>
+                </div>
+            </div>
+            <hr className='w-[100%] hidden md:flex ' />
+            <nav className={`relative top-0 left-0 w-full z-50 opacity-100 transition-colors duration-300`} id='home'>
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="flex items-center justify-between h-16">
+                        <div className="flex items-center">
+                            <a href="#">
+                                <Image src={icon} alt="icon" width={30} height={30} />
+                            </a>
+                            <div className="ml-2 md:block">
+                                <a className="text-white text-xl font-bold" href="#">Jagsco Interiors</a>
+                            </div>
+                        </div>
+                        <div className="hidden xl:flex md:ml-6">
+                            <ul className="flex space-x-4">
+                                <li>
+                                    <Link href="#" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out m-4">Furnishing</Link>
+                                </li>
+                                <li>
+                                    <Link href="#" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out m-4">Modules</Link>
+                                </li>
+                                <li>
+                                    <Link href="#" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out m-4">Wardrobes</Link>
+                                </li>
+                                <li>
+                                    <Link href="#" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out m-4">Home Decor</Link>
+                                </li>
+                                <li>
+                                    <Link href="#" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out m-4">Appliances</Link>
+                                </li>
+                                <li>
+                                    <Link href="#" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out m-5">Accessories</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="xl:hidden">
+                            <button
+                                onClick={toggleMenu}
+                                className="text-white focus:outline-none"
+                            >
+                                <img src="https://img.icons8.com/?size=100&id=8113&format=png&color=FFFFFF" alt="logo" width={20} />
+                            </button>
                         </div>
                     </div>
-                    <div className="hidden lg:flex md:ml-6">
-                        <ul className="flex space-x-4">
-                            <li>
-                                <Link href="#home" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out m-5">Home</Link>
-                            </li>
-                            <li>
-                                <Link href="#context" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out m-5">Services</Link>
-                            </li>
-                            <li>
-                                <Link href="#about" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out m-5">About Us</Link>
-                            </li>
-                            <li>
-                                <Link href="#projects" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out m-5">Projects</Link>
-                            </li>
-                            <li>
-                                <Link href="#brands" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out m-5">Brands</Link>
-                            </li>
-                            <li>
-                                <Link href="#contact" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out m-5">Contact</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="lg:hidden">
+                </div>
+                <div className={`xl:hidden bg-black bg-opacity-90 fixed inset-0 z-50 transition-opacity duration-300 ${menuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+                    <div className="flex justify-end h-16 items-center px-4">
                         <button
                             onClick={toggleMenu}
                             className="text-white focus:outline-none"
                         >
-                            <img src="https://img.icons8.com/?size=100&id=8113&format=png&color=FFFFFF" alt="logo" width={20} />
+                            <img src="https://img.icons8.com/?size=100&id=ZV8D2YZ6852I&format=png&color=FFFFFF" width={30} />
                         </button>
                     </div>
+                    <div className="flex flex-col items-center justify-center h-full">
+                        <ul className="flex flex-col items-center space-y-10 justify-center">
+                            <li className="animate-slide-in">
+                                <Link href="#" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out">Furnishing</Link>
+                            </li>
+                            <li className="animate-slide-in">
+                                <Link href="#" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out">Modules</Link>
+                            </li>
+                            <li className="animate-slide-in">
+                                <Link href="#" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out">Home Decor</Link>
+                            </li>
+                            <li className="animate-slide-in">
+                                <Link href="#" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out">Wardrobes</Link>
+                            </li>
+                            <li className="animate-slide-in">
+                                <Link href="#" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out">Appliances</Link>
+                            </li>
+                            <li className="animate-slide-in">
+                                <Link href="#" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out">Accessories</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div className={`lg:hidden bg-black bg-opacity-90 fixed inset-0 z-50 transition-opacity duration-300 ${menuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-                <div className="flex justify-end h-16 items-center px-4">
-                    <button
-                        onClick={toggleMenu}
-                        className="text-white focus:outline-none"
-                    >
-                        <img src="https://img.icons8.com/?size=100&id=ZV8D2YZ6852I&format=png&color=FFFFFF" width={30} />
-                    </button>
-                </div>
-                <div className="flex flex-col items-center justify-center h-full">
-                    <ul className="flex flex-col items-center space-y-10 justify-center">
-                        <li className="animate-slide-in">
-                            <Link href="#home" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out">Home</Link>
-                        </li>
-                        <li className="animate-slide-in">
-                            <Link href="#context" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out">Services</Link>
-                        </li>
-                        <li className="animate-slide-in">
-                            <Link href="#about" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out">About Us</Link>
-                        </li>
-                        <li className="animate-slide-in">
-                            <Link href="#projects" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out">Projects</Link>
-                        </li>
-                        <li className="animate-slide-in">
-                            <Link href="#brands" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out">Brands</Link>
-                        </li>
-                        <li className="animate-slide-in">
-                            <Link href="#contact" className="text-white hover:text-opacity-60 transition duration-300 ease-in-out">Contact</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     );
 };
 
