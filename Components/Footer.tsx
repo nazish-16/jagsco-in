d5c279a011113f0f33c40dc41d876c42933fb85f
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2024-07-19 02:36:08 +0530

    copyright

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,32 +1,38 @@
-import React from 'react'
-import icon from '../app/images/icon.png'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from 'react';
+import icon from '../app/images/icon.png';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
+    const getCurrentYear = () => new Date().getFullYear();
+
     return (
-        <div>
-            <div className='flex flex-col md:flex-row pr-[100px] pl-[100px] mt-10 bg-[#ececec] items-center justify-center xl:justify-between'>
+        <div className="bg-[#ececec]">
+            <div className='flex flex-col md:flex-row pr-[100px] pl-[100px] mt-10 items-center justify-center xl:justify-between'>
                 <div>
                     <div className='items-center xl:items-start flex flex-col justify-center'>
                         <a href="/">
                             <Image src={icon} alt="icon" className='w-[200px] relative top-8' />
                         </a>
-                        <p className='pt-3 xl:text-start max-w-lg text-center relative bottom-24' >"Elevate your space with curated home decor that inspires comfort, style, and personal expression."</p>
+                        <p className='pt-3 xl:text-start max-w-lg text-center relative bottom-24'>
+                            "Elevate your space with curated home decor that inspires comfort, style, and personal expression."
+                        </p>
                     </div>
-                    <div className='flex flex-row pt-5 items-center justify-center xl:items-start xl:justify-start relative bottom-24'>
-                        <a href="https://www.instagram.com">
-                            <img src="https://img.icons8.com/?size=100&id=32292&format=png&color=000000" alt="insta" width={30} className='m-4 hover:translate-y-[-2px] transition-all' />
-                        </a>
-                        <a href="https://www.facebook.com">
-                            <img src="https://img.icons8.com/?size=100&id=118468&format=png&color=000000" alt="fb" width={30} className='m-4 hover:translate-y-[-2px] transition-all' />
-                        </a>
-                        <a href='https://wa.me/'>
-                            <img src="https://img.icons8.com/?size=100&id=16712&format=png&color=000000" alt="whatsapp" width={30} className='m-4 cursor-pointer hover:translate-y-[-2px] transition-all' />
-                        </a>
+                    <div>
+                        <div className='flex flex-row pt-5 items-center justify-center xl:items-start xl:justify-start relative bottom-24'>
+                            <a href="https://www.instagram.com">
+                                <img src="https://img.icons8.com/?size=100&id=32292&format=png&color=000000" alt="insta" width={30} className='m-4 hover:translate-y-[-2px] transition-all' />
+                            </a>
+                            <a href="https://www.facebook.com">
+                                <img src="https://img.icons8.com/?size=100&id=118468&format=png&color=000000" alt="fb" width={30} className='m-4 hover:translate-y-[-2px] transition-all' />
+                            </a>
+                            <a href='https://wa.me/'>
+                                <img src="https://img.icons8.com/?size=100&id=16712&format=png&color=000000" alt="whatsapp" width={30} className='m-4 cursor-pointer hover:translate-y-[-2px] transition-all' />
+                            </a>
+                        </div>
                     </div>
                 </div>
-                <div className='hidden xl:flex flex-col items-center justify-center p-5 xl:p-0 '>
+                <div className='hidden xl:flex flex-col items-center justify-center p-5 xl:p-0'>
                     <h1 className='text-2xl text-center font-medium text-[#064747]'>Interiors</h1>
                     <Link href="/" className='mt-4 transition ease hover:underline hover:underline-offset-1'>Cabinet</Link>
                     <Link href="/" className='mt-4 transition ease hover:underline hover:underline-offset-1'>Cupboards</Link>
@@ -55,8 +61,13 @@ const Footer = () => {
                     <Link href="/" className='mt-4 transition ease hover:underline hover:underline-offset-1'>Freestanding</Link>
                 </div>
             </div>
-        </div >
-    )
+            <div className='bg-[#dcdcdc] py-4 text-center'>
+                <p className='text-sm text-[#064747]'>
+                    &copy; {getCurrentYear()} Jagsco Interiors. All rights reserved.
+                </p>
+            </div>
+        </div>
+    );
 }
 
-export default Footer
+export default Footer;
