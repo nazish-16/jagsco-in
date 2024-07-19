fa7b5f719f795bbf7d11052b98ace63f7f03062b
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2024-07-20 02:40:08 +0530

    home decor icon

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
             <Navbar />
             <div className='flex flex-col md:flex-row items-center justify-center p-16 md:p-28'>
                 <div>
-                    <img src='https://img.icons8.com/?size=100&id=JPXgwa11mVgS&format=png&color=ffffff' alt="#" className='w-28 h-28 md:w-32 md:h-32 mr-0 md:mr-20 lg:mr-10' />
+                    <img src='https://img.icons8.com/?size=100&id=JPXgwa11mVgS&format=png&color=ffffff' alt="#" className='w-24 h-24 md:w-28 md:h-28 mr-0 md:mr-20 lg:mr-10' />
                 </div>
                 <div className='flex flex-col'>
                     <h1 className='text-3xl sm:text-4xl text-[#cfcfcf] font-medium tracking-tighter text-center m-5 md:text-start'>Home Decor</h1>
